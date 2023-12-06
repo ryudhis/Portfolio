2245e877c6172c5f864cc03758bd7d3211ff12c8
Arya Yudhistira <ryudhis.id@gmail.com>
2023-12-07 01:19:54 +0700

    fix footer

--- portfolio/src/app/components/Footer.jsx
+++ portfolio/src/app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer border border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+    <footer className="footer border border-t-[#33353F] border-l-transparent border-r-transparent border-b-transparent text-white">
       <div className="container p-12 flex justify-between">
         <span className="text-white text-2xl">Ryudhis</span>
         <p className="text-slate-500">All rights reserved.</p>
