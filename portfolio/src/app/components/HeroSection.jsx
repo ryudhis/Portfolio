7df94c7a6ac04a0b06fc385b6654ec648e266cf0
Ryudhis <ryudhis.id@gmail.com>
2024-08-13 13:34:59 +0700

    UPDATE CV

--- portfolio/src/app/components/HeroSection.jsx
+++ portfolio/src/app/components/HeroSection.jsx
@@ -30,7 +30,7 @@ const HeroSection = () => {
                 Hire Me
               </button>
             </Link>
-            <a href="CV-AryaYudhistira.pdf" download="CV.pdf">
+            <a href="AryaYudhistira-CV.pdf" download="CV.pdf">
               <button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
                 <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                   Download CV
